2951b0e5b9b7111a107c90e841bac339ed2d1192
skuljcek09 <105319754+skuljcek09@users.noreply.github.com>
2022-07-01 10:48:00 +0200

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">Galkin store</Link>
+        <Link href="/">TWIING</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
